e74b98e836f4a56a2de7980669432e053c441196
Yehor Tolstiakov <88378124+Alf-540@users.noreply.github.com>
2024-03-24 23:21:44 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,9 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import Description from './components/Description/Description.jsx';
 import Options from './components/Options/Options.jsx';
-import Feedback from './components/Feedback/Feedback.jsx';
-import Notification from './components/Notification/Notification.jsx';
 
 function App() {
-  const initialFeedback = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  const [types, setTypes] = useState(() => {
-    const savedTypes = window.localStorage.getItem('saved-types');
-    if (savedTypes) {
-      return JSON.parse(savedTypes);
-    }
-    return initialFeedback;
-  });
-
-  useEffect(() => {
-    window.localStorage.setItem('saved-types', JSON.stringify(types));
-  }, [types]);
-
-  const updateFeedback = feedbackType => {
-    setTypes({
-      ...types,
-      [feedbackType]: types[feedbackType] + 1,
-    });
-  };
-
-  const totalFeedback = types.good + types.neutral + types.bad;
-
-  const updateTotal = () => {
-    setTypes({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-  };
-
-  const positiveStatistics = Math.round(
-    ((types.good + types.neutral) / totalFeedback) * 100
-  );
-
-  return (
-    <>
-      <Description />
-      <Options
-        updateFeedback={updateFeedback}
-        totalFeedback={totalFeedback}
-        updateTotal={updateTotal}
-      />
-      {totalFeedback <= 0 ? (
-        <Notification />
-      ) : (
-        <Feedback
-          types={types}
-          totalFeedback={totalFeedback}
-          positiveStatistics={positiveStatistics}
-        />
-      )}
-    </>
-  );
+  return <Options></Options>;
 }
 
 export default App;
