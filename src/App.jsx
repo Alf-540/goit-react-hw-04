0590c1ff19aa19224d5d3d185c5668cf6da93a8f
Yehor Tolstiakov <88378124+Alf-540@users.noreply.github.com>
2024-04-07 15:24:31 +0300

    initial commit
    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,19 @@
-import ContactList from './components/ContactList/ContactList.jsx';
-import initialContacts from './../Phonebook.json';
-import ContactForm from './components/ContactForm/ContactForm.jsx';
-import SearchBox from './components/SearchBox/SearchBox.jsx';
+import { useState, useEffect } from 'react';
+
+import SearchBar from './components/SearchBar/SearchBar.jsx';
 
 import css from './App.module.css';
 
-import { useState, useEffect } from 'react';
-
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    const savedContacts = window.localStorage.getItem('saved-contacts');
-    if (savedContacts) {
-      return JSON.parse(savedContacts);
-    }
-    return initialContacts;
-  });
-  const [filter, setFilter] = useState('');
+  const [, set] = useState();
 
-  const addContact = newContact => {
-    setContacts(contacts => {
-      return [...contacts, newContact];
-    });
-  };
-
-  const deleteContact = contactId => {
-    setContacts(contacts => {
-      return contacts.filter(contact => contact.id !== contactId);
-    });
-  };
-  useEffect(() => {
-    window.localStorage.setItem('saved-contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const visibleContact = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  useEffect();
 
   return (
     <div className={css.container}>
-      <h1>Phonebook</h1>
-      <ContactForm onAddContact={addContact} />
-      <SearchBox value={filter} onFilter={setFilter} />
-      <ContactList contacts={visibleContact} onDelete={deleteContact} />
+      {/* <ContactForm onAddContact={addContact} /> */}
+      <SearchBar value={filter} onFilter={setFilter} />
+      {/* <ContactList contacts={visibleContact} onDelete={deleteContact} /> */}
     </div>
   );
 }
