7a1d178ed9ae7ff2faf1153c631f3c703374bb3f
Yehor Tolstiakov <88378124+Alf-540@users.noreply.github.com>
2024-04-14 16:06:28 +0300

    css changes

--- src/Services/API.js
+++ src/Services/API.js
@@ -4,7 +4,7 @@ const API_KEY = 'O4fQ_gP_tqXUqYUshcB2vZNcRP-zW02dafTfTTzbmVE';
 axios.defaults.baseURL = 'https://api.unsplash.com/';
 axios.defaults.headers.common['Authorization'] = `Client-ID ${API_KEY}`;
 
-export const getPhotos = async (query, page = 1, per_page = 15) => {
+export const getPhotos = async (query, page = 1, per_page = 12) => {
   const { data } = await axios.get('search/photos', {
     params: {
       query,
